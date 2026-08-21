6bec2c04a93af240d4a89950720ac4fe78876a05
Behnam darvish <shekardarvish@gmail.com>
2026-08-21 05:39:55 -0600

    commit-16

--- app.js
+++ app.js
@@ -2,12 +2,16 @@ let div = 1;
 let div2 = 2;
 let div3 = 3;
 let div4 = 4;
+let div5 = 5;
+
 
 
 console.log(div);
 console.log(div2);
 console.log(div3);
 console.log(div4);
+console.log(div5);
+
 
 
 

fd2eff93d9fe3b9ab1e001c8fcb57f9dca867cc9
Behnam darvish <shekardarvish@gmail.com>
2026-08-21 07:27:17 -0600

    commit-2 on new-branch-4

--- app.js
+++ app.js
@@ -2,12 +2,14 @@ let div = 1;
 let div2 = 2;
 let div3 = 3;
 let div4 = 4;
+let mydiv = 20;
 
 
 console.log(div);
 console.log(div2);
 console.log(div3);
 console.log(div4);
+console.log(mydiv);
 
 
 
